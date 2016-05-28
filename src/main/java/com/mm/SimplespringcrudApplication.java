d32f46488fbe046ba30ccc703c8c71a46652ccbc
Milos Milic <mmilic@gmail.com>
2016-05-28 16:54:56 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/mm/SimplespringcrudApplication.java
@@ -0,0 +1,12 @@
+package com.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplespringcrudApplication.class, args);
+	}
+}

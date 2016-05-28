639be72a4481b902f5c4ee7d9d301eb1a2a9d575
Milos Milic <mmilic@gmail.com>
2016-05-28 17:58:57 +0200

    Added login method

--- src/main/java/com/mm/repository/UserRepository.java
+++ src/main/java/com/mm/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.mm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mm.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long>  {
-
+	
+	@Query("SELECT u from User u WHERE u.username = ?1 AND u.password = ?2")
+	User login(String username, String password);
 }

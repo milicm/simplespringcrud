9a60ba1bbcda02ff1f6603aff7d70c165b46dcb9
Milos Milic <mmilic@gmail.com>
2016-05-28 17:22:53 +0200

    Renamed Spting Boot class

--- src/main/java/com/mm/SimpleSpringCrudApplication.java
+++ src/main/java/com/mm/SimpleSpringCrudApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimplespringcrudApplication {
+public class SimpleSpringCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimplespringcrudApplication.class, args);
+		SpringApplication.run(SimpleSpringCrudApplication.class, args);
 	}
 }
